9a911a3b4e667ceed3b528b1558a0df3b44b8a75
pikuldorota <pikuldorota@gmail.com>
2019-02-04 09:50:51 +0100

    Improve verbosity, add logging to stderr and refactor CSV_report_processing

--- source/CSV_report_processing.py
+++ source/CSV_report_processing.py
@@ -1,7 +1,18 @@
 import os
+import sys
 import argparse
-import pandas as pd
 import pycountry
+import pandas as pd
+
+
+def read_file(input_file):
+    if not os.path.isfile(input_file):
+        critical("Input file doesn't exist: {}".format(input_file))
+    try:
+        file = pd.read_csv(input_file, names=['date', 'state name', 'impressions', 'CTR'])
+    except UnicodeDecodeError:
+        file = pd.read_csv(input_file, names=['date', 'state name', 'impressions', 'CTR'], encoding='utf-16')
+    return file
 
 
 def add_country_code(row):
@@ -21,6 +32,15 @@ def process(group):
     clicks = round((group[1]['CTR'].str.rstrip('%').astype(float)/100.0*group[1]['impressions']).sum())
     return [date, country_code, impressions, clicks]
 
+
+def save_data(output_file, input_file):
+    if os.path.isfile(output_file) and os.path.getsize(output_file):
+        critical("Output file exists and is not empty")
+    if input_file == output_file:
+        warning("Input and output file is the same file")
+    output.to_csv(output_file, index=False, header=None, encoding="utf-8", line_terminator='\n')
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process CSV file to AdTech platform format')
     parser.add_argument('input_file', help='path to CSV file for processing', nargs='?', default='../data/utf16.csv')
@@ -32,40 +52,42 @@ if __name__ == "__main__":
     if args.verbose:
         def critical(msg):
             print('CRITICAL: {}'.format(msg))
+            print('CRITICAL: {}'.format(msg), file=sys.stderr)
+
         def warning(msg):
             print('WARNING: {}'.format(msg))
+            print('WARNING: {}'.format(msg), file=sys.stderr)
+
+        def info(msg):
+            print('INFO: {}'.format(msg))
+
     else:
         def critical(msg):
-            pass
+            print('CRITICAL: {}'.format(msg), file=sys.stderr)
+
         def warning(msg):
+            print('WARNING: {}'.format(msg), file=sys.stderr)
+
+        def info(_):
+            """With verbosity off information won't be shown anywhere"""
             pass
 
-    if not os.path.isfile(args.input_file):
-        critical("Input file doesn't exist: {}".format(args.input_file))
-    try:
-        data = pd.read_csv(args.input_file, names=['date', 'state name', 'impressions', 'CTR'])
-    except UnicodeDecodeError:
-        data = pd.read_csv(args.input_file, names=['date', 'state name', 'impressions', 'CTR'], encoding='utf-16')
+    info('read file')
+    data = read_file(args.input_file)
+
+    info('find country codes')
     data['country code'] = data.apply(add_country_code, axis=1)
+
+    info('group data')
     grouped = data.groupby(['date', 'country code'])
+
+    info('process grouped data')
     processed = map(process, grouped)
     output = pd.DataFrame(processed)
     output[0] = pd.to_datetime(output[0])
+
+    info('sort data')
     output.sort_values([0, 1])
 
-    if os.path.isfile(args.output_file) and os.path.getsize(args.output_file):
-        critical("Output file exists and is not empty")
-    if args.input_file == args.output_file:
-        warning("Input and output file is the same file")
-    output.to_csv(args.output_file, index=False, header=None, encoding="utf-8", line_terminator='\n')
-
-
-# output_path to not empty file => warning
-# wrong type of value in cell => warning about omitting
-# strange number of columns in any row => warning
-# some checking for headers
-# errors to stderr - anything strange in file
-# add requirements file
-# accepts only csv with commas as seperator
-# make verbose more verbose
-# add logging to stderr
+    info('save data')
+    save_data(args.output_file, args.input_file)
